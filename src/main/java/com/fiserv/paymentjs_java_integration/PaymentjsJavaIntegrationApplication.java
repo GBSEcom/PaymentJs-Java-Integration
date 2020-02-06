93ded43b403b8b968f192daa5ac49c8df1051612
Michael Lynch <40207207+MichaelLynch1996@users.noreply.github.com>
2020-02-06 15:56:38 -0500

    Spring init

---
+++ src/main/java/com/fiserv/paymentjs_java_integration/PaymentjsJavaIntegrationApplication.java
@@ -0,0 +1,15 @@
+package com.fiserv.paymentjs_java_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentjsJavaIntegrationApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PaymentjsJavaIntegrationApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/fiserv/paymentjs_java_integration/PaymentjsJavaIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiserv.paymentjs_java_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentjsJavaIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
